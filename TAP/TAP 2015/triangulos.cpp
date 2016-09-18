5fcc7539d1041a264714761ad6cc76ebf0febce5
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2016-09-18 14:44:02 -0500

    organizing folders

--- TAP/TAP 2015/triangulos.cpp
+++ TAP/TAP 2015/triangulos.cpp
@@ -7,28 +7,25 @@ bool find(long long m) {
   long long usados = 0, act = 0;
   for (long long i = 1; i <= 2 * l - 1; i += 2) {
     act += i;
-    if (act >= m){
+    if (act >= m) {
       usados++;
       act = 0;
     }
-    if (usados >= k)
-      return true;
+    if (usados >= k) return true;
   }
   return usados >= k;
 }
 
 int main() {
   cin >> l >> k;
-
   long long total = 1, area = l * l;
 
   while (total < area) {
     long long medio = (total + area + 1) / 2;
-    if (find(medio))
-      total = medio;
-    else
-      area = medio - 1;
+    if (find(medio)) total = medio;
+    else area = medio - 1;
   }
+
   cout << total << endl;
   return 0;
 }
